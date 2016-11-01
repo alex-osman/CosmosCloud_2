d2904787f8503c8553a1b3b78124b614f7c68ef2
Alex Osman <aro39@drexel.edu>
2016-11-01 19:38:41 -0400

    jfieowj

--- merge.sh
+++ merge.sh
@@ -7,7 +7,7 @@ fi
 git config credential.helper "store --file=.git/credentials"
  echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
-git checkout master
+git checkout masster
 git pull origin master
 git merge $TRAVIS_COMMIT
 git push origin master
