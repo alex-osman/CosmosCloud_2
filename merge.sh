a953ebf910931c70cd8c0238fd6fc28ed7b2bd72
Alex Osman <aro39@drexel.edu>
2016-11-01 19:36:17 -0400

    move to merge

--- merge.sh
+++ merge.sh
@@ -4,6 +4,9 @@ if [ "$TRAVIS_BRANCH" != "test" ]; then
 	exit 0;
 fi
 
+git config credential.helper "store --file=.git/credentials"
+ echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+
 git checkout master
 git pull origin master
 git merge $TRAVIS_COMMIT
