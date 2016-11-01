4ab214268eea1b718f83cd0247397c1bdcd67988
jsw76 <jonathansw@outlook.com>
2016-11-01 19:47:35 -0400

    new merge try

--- merge.sh
+++ merge.sh
@@ -4,10 +4,9 @@ if [ "$TRAVIS_BRANCH" != "test" ]; then
 	exit 0;
 fi
 
-git config credential.helper "store --file=.git/credentials"
- echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+export GIT_COMMITTER_EMAIL='travis@travis'
+export GIT_COMMITTER_NAME='Travis CI'
 
-git checkout master
-git pull origin master
-git merge $TRAVIS_COMMIT -m "merge from travis test2"
+git checkout master || exit
+git merge $TRAVIS_COMMIT || exit
 git push origin master
