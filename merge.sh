693c9cf65fcd989cc221b45807c8a40fd6ff76fa
Alex Osman <aro39@drexel.edu>
2016-11-02 15:25:00 -0400

    4?

--- merge.sh
+++ merge.sh
@@ -6,9 +6,11 @@ fi
 
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-git fetch
 export GIT_COMMITTER_EMAIL='travis@travis'
 export GIT_COMMITTER_NAME='Travis CI'
+
+git remote update
+git fetch
 
 git checkout -t -b master origin/master
 git merge $TRAVIS_COMMIT || exit
