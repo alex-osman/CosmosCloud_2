26a3d35245fdc6865bd442648882d2ee70f8772a
Alex Osman <aro39@drexel.edu>
2016-11-02 14:52:27 -0400

    merge updated to fetch

--- merge.sh
+++ merge.sh
@@ -5,8 +5,9 @@ if [ "$TRAVIS_BRANCH" != "test" ]; then
 fi
 
 git config credential.helper "store --file=.git/credentials"
- echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
+git fetch
 git checkout master
 git pull origin master
 git merge $TRAVIS_COMMIT -m "merge from travis test2"
