71afe97a50dd0f5f9e37afa0b5dcb1bdb2fb056f
Alex Osman <aro39@drexel.edu>
2016-11-03 11:20:48 -0400

    cd into temp before entering credentials

--- merge.sh
+++ merge.sh
@@ -3,6 +3,9 @@
 if [ "$TRAVIS_BRANCH" != "test" ]; then
 	exit 0;
 fi
+echo "cloning and cd temp"
+git clone "https://github.com/alex-osman/CosmosCloud_SD" "temp"
+cd "temp"
 
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
@@ -11,9 +14,6 @@ export GIT_COMMITTER_NAME='Travis CI'
 
 push_uri="https://$GH_TOKEN@github.com/CosmosCloud_SD"
 
-echo "cloning and cd temp"
-git clone "https://github.com/alex-osman/CosmosCloud_SD" "temp"
-cd "temp"
 
 echo "checking out master"
 git checkout master
