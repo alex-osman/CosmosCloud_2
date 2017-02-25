c6984fc14ef154405a76523736568ccfae2a4cb6
jsw76 <jonathansw@outlook.com>
2017-02-25 18:00:51 -0500

    added rgb.js

---
+++ routes/rgb.js
@@ -0,0 +1,77 @@
+var assert = require('assert');
+
+module.exports = function(baseUrl, app, request, MongoClient) {
+  /*
+   * Send the :style to a specific :node
+   * @param {String} :node - The node that we want to communicate with
+   * @param {String} :style - The style for the indicator
+   * @return {TODO} TODO
+  */
+  app.get(baseUrl + '/:node/:style', function(req, res) {
+    var style = req.params.style;
+    getPort(function(ports) {
+      getRgbIP(function(rgbIP) {
+        rgbIP.forEach(function(ip) {
+          //  console.log('http://' + ip.ip + ':' + ports.rgb + '/' + style);
+          request('http://' + ip.ip + ':' + ports.rgb + '/' + style, function(err, response, body) {
+            if (!err && response.statusCode === 200) {
+              //  console.log(body);
+              res.send(body);
+            } else {
+              console.log(err);
+            }
+          });
+        });
+      });
+    });
+  });
+
+  /*
+   * Send the :style to a specific :node
+   * @param {String} :node - The node that we want to communicate with
+   * @param {String} :style - The style for the indicator
+   * @param {String} :red - The rgb color code for red
+   * @param {String} :green - The rgb color code for green
+   * @param {String} :blue - The rgb color code for blue
+   * @return {TODO} TODO
+  */
+  app.get(baseUrl + '/:node/:style/:red/:green/:blue', function(req, res) {
+    var style = req.params.style;
+    var r = req.params.red;
+    var g = req.params.green;
+    var b = req.params.blue;
+    getPort(function(ports) {
+      getRgbIP(function(rgbIP) {
+        rgbIP.forEach(function(ip) {
+          //  console.log('http://' + ip.ip + ':' + ports.rgb + '/' + style + '/' + r +'/' + g + '/' + b);
+          request('http://' + ip.ip + ':' + ports.rgb + '/' + style + '/' + r + '/' + g + '/' + b, function(err, response, body) {
+            if (!err && response.statusCode === 200) {
+              //  console.log(body);
+              res.send(body);
+            } else {
+              console.log(err);
+            }
+          });
+        });
+      });
+    });
+  });
+};
+
+//  Get all the Ips that have a RGB module tied to them
+var getRgbIP = function(callback) {
+  var collection = db.collection('nodes');
+  collection.find({'modules.type': 'rgb'}).toArray(function(err, ip) {
+    assert.equal(err, null);
+    callback(ip);
+  });
+};
+
+//  Get the ports for specific modules
+var getPort = function(callback) {
+  var collection = db.collection('ports');
+  collection.findOne({}, function(err, ports) {
+    assert.equal(err, null);
+    callback(ports);
+  });
+};
