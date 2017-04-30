e105e697a8ea6758cd340df0e864d8deb93ad34e
jsw76 <jonathansw@outlook.com>
2017-04-30 19:36:31 -0400

    relay page is connected but does not populate because I didnt test on with node running

--- src/app/layout/relay/relay.module.ts
+++ src/app/layout/relay/relay.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { RelayRoutingModule } from './relay-routing.module';
 import { RelayComponent } from './relay.component';
+import { NodeService } from './../node.service';
+import { RelayService } from './../relay.service';
 
 @NgModule({
   imports: [
     CommonModule,
     RelayRoutingModule
   ],
+  providers: [NodeService, RelayService],
   declarations: [RelayComponent]
 })
 export class RelayModule { }
