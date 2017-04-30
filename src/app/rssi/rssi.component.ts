559bf2561dfdcb037a60e698fac3ae7e0f847a1e
galacticpenguin <galacticpengu1n@outlook.com>
2017-04-30 15:56:15 -0400

    Updated code still not working

--- src/app/rssi/rssi.component.ts
+++ src/app/rssi/rssi.component.ts
@@ -3,7 +3,6 @@ import { Relay } from '../relay';
 import { Channel } from '../channel';
 import { Node } from '../node';
 import { NodeService } from '../node.service';
-import { RelayService } from '../relay.service';
 
 @Component({
   moduleId: module.id,
@@ -13,17 +12,22 @@ import { RelayService } from '../relay.service';
 })
 
 export class RSSIComponent implements OnInit {
-	nodes: Node[];
+  nodes: Node[];
+  channels: Channel[];
 
-	constructor(private nodeService: NodeService) { }
+  constructor(private nodeService: NodeService) { }
 
     ngOnInit() {
- 		this.getNodes();
- 	}
+    this.getNodes();
+  }
 
-  	getNodes(): void {
-    	this.nodeService.getNodes().then(nodes => this.nodes = nodes);
-	}
+    getNodes(): void {
+      this.nodeService.getNodes().then(nodes => this.nodes = nodes);
+  }
+
+  onChange(value) {
+    this.nodes = value;
+  }
 
 
 }
