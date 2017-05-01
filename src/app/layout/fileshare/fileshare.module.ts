7ef9bf2c07242c3614ecaa4d18ad3684dd2e51a0
jsw76 <jonathansw@outlook.com>
2017-05-01 18:16:07 -0400

    things show up now :D

--- src/app/layout/fileshare/fileshare.module.ts
+++ src/app/layout/fileshare/fileshare.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { FileshareRoutingModule } from './fileshare-routing.module';
 import { FileshareComponent } from './fileshare.component';
+import { FileSelectDirective, FileDropDirective, FileUploader } from 'ng2-file-upload/ng2-file-upload';
+import { FileService } from './../file.service';
 
 @NgModule({
   imports: [
     CommonModule,
     FileshareRoutingModule
   ],
-  declarations: [FileshareComponent]
+  providers: [FileService],
+  declarations: [FileshareComponent, FileSelectDirective, FileDropDirective,]
 })
 export class FileshareModule { }
--- src/app/layout/indicator/indicator.module.ts
+++ src/app/layout/indicator/indicator.module.ts
@@ -3,12 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { IndicatorRoutingModule } from './indicator-routing.module';
 import { IndicatorComponent } from './indicator.component';
+import { NodeService } from './../node.service';
+import { RelayService } from './../relay.service';
+import { IndicatorService } from './../indicator.service';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
   imports: [
     CommonModule,
-    IndicatorRoutingModule
+    IndicatorRoutingModule,
+    ColorPickerModule
   ],
+  providers: [NodeService, RelayService, IndicatorService],
   declarations: [IndicatorComponent]
 })
 export class IndicatorModule { }
