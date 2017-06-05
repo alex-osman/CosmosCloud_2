063b1d98c8a8ffb23c53e483d19ae7a1d1b7656b
jsw76 <jonathansw@outlook.com>
2017-06-04 22:47:40 -0400

    ehh

--- src/app/layout/alarm/alarm.component.spec.ts
+++ src/app/layout/alarm/alarm.component.spec.ts
@@ -1,36 +1,58 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { DebugElement, destroyPlatform } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ColorPickerModule } from 'angular2-color-picker';
 
 import { AlarmComponent } from './alarm.component';
+import { AlarmService } from '../alarm.service';
+import { Alarm } from '../alarm';
+import { TriggerModule } from './../../shared';
 
-describe('AlarmComponent', () => {
-  let component: AlarmComponent;
+let newAlarm = new Alarm();
+newAlarm = {
+  '_id': '1',
+  'name': 'Living Room',
+  'trigger': ['Kitchen'],
+  'triggerString': ['Relay'],
+  'cronDate': '12:00AM',
+  'active': true
+};
+
+
+class MockAlarmService {
+  public getAlarms(): Promise<Alarm[]> {
+    const alarms: Alarm[] = new Array<Alarm> (newAlarm);
+
+    return Promise.resolve(alarms);
+  }
+}
+
+describe('Component: AlarmComponent', () => {
   let fixture: ComponentFixture<AlarmComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AlarmComponent ]
-    })
-    .compileComponents();
-  }));
+  let alarmService: AlarmService;
+  let component: AlarmComponent;
+  let de: DebugElement;
+  let el: HTMLElement;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ FormsModule, TriggerModule, ColorPickerModule ],
+      declarations: [ AlarmComponent ],
+      providers: [ {provide: AlarmService, useClass: MockAlarmService} ],
+    });
     fixture = TestBed.createComponent(AlarmComponent);
     component = fixture.componentInstance;
+    alarmService = TestBed.get(AlarmService);
+    de = fixture.debugElement;
+    el = de.nativeElement;
     fixture.detectChanges();
   });
 
-/*
-  it('should create an alarm component', () => {
-    let app = new AlarmComponent();
-    expect(app).toBeTruthy();
-  });
-*/
-
-  it('should have defined component', () => {
+  it('should create component', () => {
     expect(component).toBeDefined();
   });
+
 
 });
